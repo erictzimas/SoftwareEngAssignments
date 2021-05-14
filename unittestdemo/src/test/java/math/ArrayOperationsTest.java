106b2588a966df0dcd61057e494d9f60933913c2
Eric Tzimas <errikos.tzimas@gmail.com>
2021-05-14 15:16:03 +0300

    Add test cases in ArrayOperationsTest.java as per #7

--- unittestdemo/src/test/java/math/ArrayOperationsTest.java
+++ unittestdemo/src/test/java/math/ArrayOperationsTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.*;
  * @author erictzimas
  */
 public class ArrayOperationsTest {
+	ArrayOperations pp = new ArrayOperations();
 	public static String resourcesPath = "src/test/resources/";
 	/*
 	 * A test case that checks a valid input file with prime and non prime numbers
@@ -32,5 +33,35 @@ public class ArrayOperationsTest {
 		int[] actual = ao.findPrimesIntFile(fileio, path, myMath);
 		
 		assertArrayEquals(expected, actual);	
+	}
+	/*
+	 * A test case that checks an invalid input file that does not exist 
+	 */
+	@Rule
+	public ExpectedException thrown = ExpectedException.none(); //initialize it to .none()
+	@Test
+	public void testFindPrimesShouldReturnNoFileExists() {
+		FileIO fileio = mock(FileIO.class);
+		MyMath myMath = mock(MyMath.class);
+		String nonvalidInputFilepath = 
+				resourcesPath.concat("filethatdoesntexist.txt");
+    	thrown.expect(IllegalArgumentException.class);
+    	thrown.expectMessage("Input file does not exist");
+    	pp.findPrimesIntFile(fileio, nonvalidInputFilepath, myMath);
+	}
+	/*
+	 * A test case that checks an invalid input file with size = 0
+	 */
+	@Rule
+	public ExpectedException thrown2 = ExpectedException.none(); //initialize it to .none()
+	@Test
+	public void testFindPrimesShouldReturnEmptyFile() {
+		FileIO fileio = mock(FileIO.class);
+		MyMath myMath = mock(MyMath.class);
+		String nonvalidInputFilepath = 
+				resourcesPath.concat("empty.txt");
+    	thrown2.expect(IllegalArgumentException.class);
+    	thrown2.expectMessage("Given file is empty");
+    	pp.findPrimesIntFile(fileio, nonvalidInputFilepath, myMath);
 	}
 }

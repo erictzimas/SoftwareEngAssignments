c6f357ea146cf2bfe21b2f78b0e11e659da79dd0
Eric Tzimas <errikos.tzimas@gmail.com>
2021-06-12 19:54:00 +0300

    Fixing travis bug

--- SourceCodeAnalyzer/src/main/java/codeanalyzer/Analyzer.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.codeanalyzer;
-
-import java.io.IOException;
-
-public interface Analyzer {
-	public int calculateLOC(String filepath) throws IOException;
-	public int calculateNOM(String filepath) throws IOException;
-	public int calculateNOC(String filepath) throws IOException;
-}
--- SourceCodeAnalyzer/src/main/java/codeanalyzer/Exporter.java
+++
@@ -1,8 +0,0 @@
-package src.main.java.codeanalyzer;
-
-import java.util.Map;
-
-public interface Exporter {
-	
-	 public void writeFile(String outputType, Map<String, Integer> metrics, String filepath);
-}
